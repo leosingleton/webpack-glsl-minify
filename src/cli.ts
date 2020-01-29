fa72c07e73e2820895b99555948caf73a0273951
Leo Singleton <leo@leosingleton.com>
2020-01-29 07:05:24 -0500

    Adding placeholder for CLI app
    This can be invoked by running "npx webpack-glsl-minify ..." or
    "npx wgm ..." for short.

---
+++ src/cli.ts
@@ -0,0 +1,5 @@
+// src/cli.ts
+// Copyright 2018-2020 Leo C. Singleton IV <leo@leosingleton.com>
+// Entry point when running "npx webpack-glsl-minify ..." on the command line
+
+console.log('Hello world!');
